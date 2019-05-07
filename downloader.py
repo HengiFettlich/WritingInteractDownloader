23e96a25e76678cd702b5311d5989cfb315637d6
hengifett <hengifett@gmail.com>
2019-05-07 06:33:35 +0200

    added some documentation, fixed error where "/" at the end would lead to empty download

--- downloader.py
+++ downloader.py
@@ -8,9 +8,12 @@ from lxml import html
 username = "nottheonion"
 password = "Password1"
 
+# Session to store login cookies
 s = requests.Session()
 
 
+# replaces the given uri in a html response. turns these into relative paths
+# there could be better ways to do that
 def replace_uris(base_resp: str, uri) -> str:
     name = uri.split("/")[::-1][0]
     base_uri = uri.replace("map/" + name, "map/")
@@ -29,23 +32,25 @@ def download_and_check(uri: str):
 
     response = "0"
 
+    # chapter name
     name = uri.split("/")[::-1][0]
 
+    # save dir, inside chapters
     path = "%s/chapters/%s.html" % (base, name)
     while not downloaded:
-
+        # don't redownload files, if running the script twice
         if os.path.isfile(path):
             return
 
         try:
             response = s.get(uri).text
-
+        # in case of SSL exceptions or something else, log these
         except Exception as e:
             with open(base + "/log.txt", "a") as l_out:
                 l_out.write(str(e) + "\n")
 
             response = "resource limitations"
-
+        # either happens if an Exception is thrown or if writing.com goofs up
         if "resource limitations" in response:
             print(name + " temporarily unavailable, trying again in 60 seconds..")
             time.sleep(60)
@@ -72,7 +77,7 @@ def download_and_check(uri: str):
 def sleep_timer(duration):
     for x in range(duration, 0, -1):
         time.sleep(1)
-        print("== Sleeping ==", x, end="\r")
+        print("== Sleeping ==", str(x).zfill(2), end="\r")
 
 
 def convert_html(delimiter, html_file, filter_str):
@@ -81,6 +86,8 @@ def convert_html(delimiter, html_file, filter_str):
     ls = t.xpath(delimiter)
 
     matches = [x for x in set(ls) if filter_str in x]
+
+    # failsafe if writing.com throws resource errors. chapter list will be empty if that happens
     if len(matches) == 0:
         print("empty response, retrying..")
         sleep_timer(duration=60)
@@ -95,19 +102,31 @@ def validate_uri(uri: str):
 
 
 def login():
+    # login is needed to view the outline
+    # default login uses generic username from bugmenot.com
     print("logging in to writing.com")
     s.post("https://www.writing.com/main/login.php",
            data={"login_username": username, "login_password": password, "send_to": "/"})
 
 
 def fetch_story(uri: str):
+    # remove last slash if present
+    if uri[::-1][0] == "/":
+        uri = uri[:-1]
+
+    if "/map/1" in uri:
+        uri = uri.replace("/map/1", "")
+
+    # base is used throughout the whole project as the base directory
     global base
+    # this should correspond to the Id and name of the story.
     base = uri.split("/")[::-1][0]
     print(base)
 
+    # creates base dir and chapter directory.
     if not os.path.exists(base):
         os.makedirs(base + "/chapters")
-
+    # TODO implement basic logging mechanics
     with open(base + "/log.txt", "w") as l_out:
         l_out.write(base + " start\n")
 
@@ -116,6 +135,7 @@ def fetch_story(uri: str):
 
     temp = s.get(entry).text
 
+    # outline link will be replaced in the chapters to make the story browsable
     global outline_link
     outline_link = convert_html("//a/@href", temp, "/outline")[0]
 
@@ -124,19 +144,23 @@ def fetch_story(uri: str):
 
     outline = s.get(outline_link).text
 
-    list_str = convert_html("//a/@href", outline, "/map/")
+    # chapter list
+    chapter_list = convert_html("//a/@href", outline, "/map/")
 
-    for x in list_str:
-        outline = replace_uris(outline, x)
+    # makes the outline browsable by replacing all chapter links with their relative path
+    for chapter in chapter_list:
+        outline = replace_uris(outline, chapter)
 
     with open(base + "/outline.html", "w") as outline_out:
         outline_out.write(outline)
 
+    # multiprocess it because why not
     pool = ThreadPool()
 
-    pool.map(download_and_check, list_str)
+    pool.map(download_and_check, chapter_list)
 
 
+# main loop
 def main():
     uris = sys.argv[1:]
     for uri in uris:
